3d8f61eca657862e3bea76a1f5054c3a9cb9f2f6
kichu12348 <rmahadevan574@gmail.com>
2024-01-13 20:52:02 +0530

    last

---
+++ app.js
@@ -0,0 +1,58 @@
+//dom variables
+
+const form = document.querySelector('form');
+const input = document.querySelector('#search');
+const temp = document.querySelector('#temp');
+const desc = document.querySelector('#desc');
+const wind = document.querySelector('#windSpeed');
+const humid = document.querySelector('#humid');
+const cityName = document.querySelector('h1');
+const disp = document.querySelector('#noItems');
+
+// see if the user has submitted the form
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const city = input.value.toLowerCase();
+
+    // check if the user has entered a city
+    if (input.value === '') {
+        errorDisplay('Please enter a city');
+    }
+    
+    else {
+
+        try{
+        // waits for fetched the data from the API
+        const data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=8JGSDFCRK5Y49TKWB37ZNQR2K`).then(response => response.json());
+       
+        // update data in the DOM from the API
+        temp.textContent = data.days[0].temp + '°C';
+        desc.textContent = data.days[0].conditions;
+        wind.textContent = data.days[0].windspeed + ' km/h';
+        cityName.textContent = data.resolvedAddress;
+        humid.textContent = data.days[0].humidity + '%';
+        disp.id = 'items'
+        input.value = '';
+        }
+
+        catch(err){
+            errorDisplay('Location not found');
+        }
+
+    };
+}
+);
+
+// function to display error message if caught
+
+function errorDisplay(message) {
+    disp.id = 'items'
+    cityName.textContent = message;
+    temp.textContent = '';
+    desc.textContent = '';
+    wind.textContent = '';
+    humid.textContent = '';
+    input.value = '';
+}
