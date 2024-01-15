12f6f58a7c029f3f1f6d55378b6672ae1142d401
kichu12348 <rmahadevan574@gmail.com>
2024-01-15 22:05:19 +0530

    times

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //dom variables
-
 const form = document.querySelector('form');
 const input = document.querySelector('#search');
 const temp = document.querySelector('#temp');
@@ -8,15 +7,16 @@ const wind = document.querySelector('#windSpeed');
 const humid = document.querySelector('#humid');
 const cityName = document.querySelector('h1');
 const disp = document.querySelector('#noItems');
+const time = document.querySelector('#times');
 
-// see if the user has submitted the form
+// check if the user has submitted the form
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const city = input.value.toLowerCase();
 
-    // check if the user has entered a city
+    // check if the user has entered a city or left blank
     if (input.value === '') {
         errorDisplay('Please enter a city');
     }
@@ -24,19 +24,37 @@ form.addEventListener('submit', async (e) => {
     else {
 
         try{
-        // waits for fetched the data from the API
-        const data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=8JGSDFCRK5Y49TKWB37ZNQR2K`).then(response => response.json());
+
+            // waits for fetched the data from the API
+            const data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=8JGSDFCRK5Y49TKWB37ZNQR2K`).then(response => response.json());
        
-        // update data in the DOM from the API
-        temp.textContent = Math.floor(data.days[0].temp) + '°C';
-        desc.textContent = data.days[0].conditions;
-        wind.textContent = 'windspeed: '+data.days[0].windspeed + ' km/h';
-        cityName.textContent = data.resolvedAddress;
-        humid.textContent ='humidity: '+ data.days[0].humidity + '%';
-        if(disp.id !== 'items'){
-            disp.id = 'items'
-        };
-        input.value = '';
+            // update data in the DOM from the API
+            temp.textContent = Math.floor(data.days[0].temp) + '°C';
+            desc.textContent = data.days[0].conditions;
+            wind.textContent = 'windspeed: '+ Math.floor(data.days[0].windspeed) + ' km/h';
+            cityName.textContent = data.resolvedAddress;
+            humid.textContent ='humidity: '+ Math.floor(data.days[0].humidity) + '%';
+            
+            //time from the API  
+            try{
+                const timezone = await fetch(`http://worldtimeapi.org/api/timezone/${data.timezone}`).then(responseT => responseT.json()).then(dataT => dataT.datetime).then(dateT => dateT.split('T')[1].split('.')[0]).then(timeT => timeT.split(':'))
+                //adds time to dom
+                time.textContent = 'Local Time: ' + timezone[0].toString().padStart(2,'0')+ ':' + timezone[1].toString().padStart(2,'0');
+
+            }
+            catch(err){
+                console.log(err +"you are gaeeeeeeeeee");
+
+            }
+
+            if(disp.id !== 'items') {
+
+                disp.id = 'items'
+
+            };
+
+            input.value = '';
+
         }
 
         catch(err){
@@ -50,13 +68,19 @@ form.addEventListener('submit', async (e) => {
 // function to display error message if caught
 
 function errorDisplay(message) {
-    if(disp.id !== 'items'){
-         disp.id = 'items'
-      }
+
+    if(disp.id !== 'items') {
+
+        disp.id = 'items'
+
+    }
+
     cityName.textContent = message;
     temp.textContent = '';
     desc.textContent = '';
     wind.textContent = '';
     humid.textContent = '';
     input.value = '';
+    time.textContent = '';
+
 }
